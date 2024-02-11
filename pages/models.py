4a878faa2e3864deea7d66f9254af512e6ed298f
rampoe <mail.rampoe@gmail.com>
2024-02-11 10:21:16 +0500

    sync

--- pages/models.py
+++ pages/models.py
@@ -11,13 +11,22 @@ class Article(models.Model):
 class Quiz(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.question.text} ({self.text})"
